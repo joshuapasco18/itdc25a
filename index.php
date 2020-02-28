ad1525563b3d1de6c0ad0f72b7447d0b64f715c2
joshuapasco18 <joshuajunp@gmail.com>
2020-02-27 18:07:44 -0800

    correct errors

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
 		<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,300italic,700,700italic">
 		<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/3.0.3/normalize.css">
 		<link rel="stylesheet" href="/dist/milligram.min.css">
-		<link rel="stylesheet" href=dist/main.css">
+		<link rel="stylesheet" href="dist/main.css">
 	</head>
 	<body>
 
